036a79523a9e2da3ecee38e194f63390b2f4af47
B3Y0NDER <dine.terzi@epitech.digital>
2025-09-02 11:35:07 +0200

    Update basic_ops.py

--- partie3/operations/basic_ops.py
+++ partie3/operations/basic_ops.py
@@ -1,5 +1,9 @@
 def add(a: int, b: int) -> int:
-    
-def subtract(a: int, b: int) -> int:
-   
+    """Retourne la somme de a et b."""
+    # TODO: retourner la somme
+    10,6
 
+def subtract(a: int, b: int) -> int:
+    """Retourne la différence entre a et b (a - b)."""
+    # TODO: retourner la différence
+    10,7
